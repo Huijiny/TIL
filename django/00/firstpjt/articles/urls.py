54b5bd218f63688a72c556812ebe785e69bc6f10
Huijiny <jiin20803@gmail.com>
2021-03-08 16:02:19 +0900

    add url namespace

--- django/00/firstpjt/articles/urls.py
+++ django/00/firstpjt/articles/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
-    path('greeting/', views.greeting),
-    path('dinner/', views.dinner),
-    path('throw/', views.throw),
-    path('catch/', views.catch),
+    path('index/', views.index, name='index'),
+    path('greeting/', views.greeting, name='greeting'),
+    path('dinner/', views.dinner, name='dinner'),
+    path('throw/', views.throw, name='throw'),
+    path('catch/', views.catch, name='catch'),
     path('hello/<str:name>/', views.hello),
     #path('hello/<name>/, views.hello), int는 생략 불가, str은 생략 가능
 ]
