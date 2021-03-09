dc0173aa9ec1150189bc5cb58383cbef2f57edd7
Huijiny <jiin20803@gmail.com>
2021-03-09 11:46:38 +0900

    variable routing

--- django/my_pjt/articles/urls.py
+++ django/my_pjt/articles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name='articles'
 urlpatterns = [
-    path('image/', views.image, name='image')
+    path('image/', views.image, name='image'),
+    path('search/', views.search, name='search'),
+    path('result/<int:param1>/<int:param2>/', views.result, name='result'),
 ]
--- django/my_pjt/articles/views.py
+++ django/my_pjt/articles/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def image(request):
     return render(request, 'articles/image.html')
+
+def search(request):
+    return render(request, 'articles/search.html')
+
+def result(request, param1, param2):
+    print(param1 * param2)
+    context = {
+        'result': param1 * param2,
+    }
+    return render(request, 'articles/result.html', context)
