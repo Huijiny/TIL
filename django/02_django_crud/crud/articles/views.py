76cd87d0bba8b8859a96049ea18aadf1ee4b7b23
Huijiny <jiin20803@gmail.com>
2021-03-11 11:07:47 +0900

    add delete method

--- django/02_django_crud/crud/articles/views.py
+++ django/02_django_crud/crud/articles/views.py
@@ -52,3 +52,11 @@ def detail(request, pk):
     }
     return render(request, 'articles/detail.html', context)
     
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method =='POST':
+        article.delete()
+        return redirect('articles:index')
+    return redirect('articles:detail', article.pk)
+
