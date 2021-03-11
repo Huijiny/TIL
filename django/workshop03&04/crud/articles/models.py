f0c73224b7b3dcc532acee783730862ff25bc725
Huijiny <jiin20803@gmail.com>
2021-03-11 11:55:38 +0900

    add admin user

--- django/workshop03&04/crud/articles/models.py
+++ django/workshop03&04/crud/articles/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=10)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_created=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
