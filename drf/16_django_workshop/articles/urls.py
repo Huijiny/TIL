66b8c9f1b211509a36e9da8473232316a3ac7347
Huijiny <jiin20803@gmail.com>
2021-04-27 16:02:36 +0900

    remove workshop folder

--- drf/16_django_workshop/articles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-
-urlpatterns = [
-    path('articles/', views.articles_list),
-    path('articles/<int:article_pk>/', views.article_detail),
-]
