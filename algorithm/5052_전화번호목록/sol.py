3280f40294bb6defae7bd9d1f9d42ba2d69aa88a
Huijiny <jiin20803@gmail.com>
2021-06-25 12:14:14 +0900

    5052 전화번호 목록

---
+++ algorithm/5052_전화번호목록/sol.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open("sample.txt")
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    numbers = []
+    for _ in range(n):
+        numbers.append(input())
+
+    numbers.sort()
+    flag = True
+    for i in range(len(numbers) - 1):
+        if numbers[i] in numbers[i + 1][:len(numbers[i])]:
+            flag = False
+            break
+
+    if flag:
+        print("YES")
+    else:
+        print("NO")
